7b93ad6425a68d90be46f21b40174fe5475fd85c
bofmar <bofmar@outlook.com>
2022-11-16 11:16:51 +0200

    Add image

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import picture from './images/pic.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <img src={picture} alt='A picture of me' className='picture' />
+
     </div>
   );
 }
