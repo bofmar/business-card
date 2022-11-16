746d77140dc61374f2139b47671acbf7615f3b97
bofmar <bofmar@outlook.com>
2022-11-16 12:42:04 +0200

    Add styling

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import Info from './componenets/Info';
 import Buttons from './componenets/Buttons';
 import About from './componenets/About';
 import Interests from './componenets/Interests';
+import Footer from './componenets/Footer';
 import picture from './images/pic.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <img src={picture} alt='A picture of me' className='picture' />
+      <div className='img-container' >
+        <img src={picture} alt='A picture of me' className='picture' />
+      </div>
       <Info />
       <Buttons />
       <About />
       <Interests />
-
-
+      <Footer />
     </div>
   );
 }
